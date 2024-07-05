2c23d805967c6ecb294615e976eef448b69d0cf2
Richard Shaju <richardshaju66@gmail.com>
2024-07-06 00:33:46 +0530

    signup

---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Page() {
+  return (
+    <div className='b'>
+        
+    </div>
+  )
+}
+
+export default Page
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Page() {
+  return (
+    <div
+      style={{
+        backgroundImage: "url('/img/bg.png')",
+      }}
+      className="flex flex-col items-center w-full h-screen bg-center bg-cover bg-no-repeat"
+    > 
+
+    <div className="flex justify-start h-full w-full">
+      <div className="w-[35%] h-full bg-white ">
+       <div className="p-10">
+        <h1>CUT MY HAIR</h1>
+       </div>
+      </div>
+    </div>
+
+
+    </div>
+  );
+}
+
+export default Page;
