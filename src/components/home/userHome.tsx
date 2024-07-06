aafe371545326f159ee09f1ace0cbff4cce91945
Muhammed Aslam <muhammedaslam204@gmail.com>
2024-07-06 23:36:05 +0530

    slot completed

--- src/components/home/userHome.tsx
+++ src/components/home/userHome.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import BasicDateCalendar from '@/app/salon/slots/SaloonSlots'
 
 function UserHome() {
   return (
     <div>
-        fskfnmkf
+        <BasicDateCalendar/>
     </div>
   )
 }
