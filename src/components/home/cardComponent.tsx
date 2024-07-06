2254a2f4cfb233c3def183b9ab3b3e5055540448
rafnas.pp <rafnas.pp@maisondauraine.com>
2024-07-06 19:13:43 +0530

    Salon List integration

--- src/components/home/cardComponent.tsx
+++ src/components/home/cardComponent.tsx
@@ -1,55 +1,73 @@
-import React from "react"
-import Image from "next/image"
-import './home.css'
-import { MapPin } from 'lucide-react';
+"use client";
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
+import "./home.css";
+import { MapPin } from "lucide-react";
+import axios from "axios";
 
 const CardComponent = () => {
+  const [salons, setSalons] = useState([]);
 
-    const cards = [
-        {
-          src: "/img/image19.png",
-          alt: "MARLON MCDONALD",
-          heading: "MARLON MCDONALD",
-          address: "Address"
-        },
-        {
-          src: "/img/image19.png",
-          alt: "MARLON MCDONALD",
-          heading: "MARLON MCDONALD",
-          address: "Address"
-        },
-        {
-          src: "/img/image19.png",
-          alt: "MARLON MCDONALD",
-          heading: "MARLON MCDONALD",
-          address: "Address"
-        }]
+  useEffect(() => {
+    axios
+      .get("https://cutmyhair.onrender.com/saloon/getSaloons")
+      .then((response) => {
+        console.log(response.data);
+        setSalons(response.data.result);
+      })
+      .catch((error) => {
+        console.error("Error fetching saloons:", error);
+      });
+  }, []);
+
+  console.log(salons, "salons");
+
+  const cards = [
+    {
+      src: "/img/image19.png",
+      alt: "MARLON MCDONALD",
+      heading: "MARLON MCDONALD",
+      address: "Address",
+    },
+    {
+      src: "/img/image19.png",
+      alt: "MARLON MCDONALD",
+      heading: "MARLON MCDONALD",
+      address: "Address",
+    },
+    {
+      src: "/img/image19.png",
+      alt: "MARLON MCDONALD",
+      heading: "MARLON MCDONALD",
+      address: "Address",
+    },
+  ];
 
   return (
-        <div className="w-full flex justify-center">
-            <div className="p-6 flex gap-8">
-        {cards.map((card, index) => (
-        <div key={index} className="card-component">
-          <div className="image-container">
-            <Image 
-              src={card.src} 
-              alt={card.alt} 
-              width={100} 
-              height={100} 
-              layout="responsive" 
-            />
-          </div>
-          <div className="bg-black text-white text-center pt-8 card-content flex flex-col pb-10">
-            <h1 className="saloon-heading text-xl">{card.heading}</h1>
-            <div className="flex gap-3 justify-center">
-            <MapPin />
-            <p className="saloon-address">{card.address}</p>
+    <div className="w-full flex justify-center">
+      <div className="p-6 flex gap-8">
+        {salons.map((card, index): any => (
+          <div key={index} className="card-component">
+            <div className="image-container">
+              <Image
+                src={card?.image}
+                alt="Image of a saloon"
+                width={10}
+                height={10}
+                layout="responsive"
+              />
+            </div>
+            <div className="bg-black text-white text-center pt-8 card-content flex flex-col pb-10">
+              <h1 className="saloon-heading text-xl">{card?.name}</h1>
+              <div className="flex gap-3 justify-center">
+                <MapPin />
+                <p className="saloon-address">{card?.location}</p>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
-        </div>
-  )
-}
-export default CardComponent
+  );
+};
+export default CardComponent;
