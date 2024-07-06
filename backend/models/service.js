09a70063f54d9bb3f06420e3f0f58ed2c352b81b
showkathali <showkathali@flexm.com>
2024-07-06 12:59:27 +0530

    updated saloon login

--- backend/models/service.js
+++ backend/models/service.js
@@ -1,10 +1,34 @@
 import mongoose from "mongoose";
 
-const saloonSchema = mongoose.Schema({
+const serviceSchema = mongoose.Schema({
   title: { type: String, required: true },
   desc: { type: String, required: true },
   price: { type: String, required: true },
   image: { type: String, required: true },
 });
 
-export default mongoose.model("saloon", saloonSchema);
+export default mongoose.model("service", serviceSchema);
+
+
+// import mongoose from 'mongoose';
+
+// const { Schema } = mongoose;
+
+// const serviceSchema = new Schema({
+//   name: {
+//     type: String,
+//     required: true,
+//   },
+//   price: {
+//     type: Number,
+//     required: true,
+//   },
+//   duration: {
+//     type: Number,
+//     required: true,
+//   },
+// });
+
+// const Service = mongoose.models.Service || mongoose.model('Service', serviceSchema);
+
+// export default Service;
