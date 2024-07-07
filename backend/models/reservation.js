0aacb175259de063d0607531003549dd50cd45f9
Richard Shaju <richardshaju66@gmail.com>
2024-07-07 10:00:33 +0530

    minor fix

--- backend/models/reservation.js
+++ backend/models/reservation.js
@@ -4,7 +4,7 @@ const reservationSchema = mongoose.Schema({
   salonId: { type: String, required: true },
   userId: { type: String, required: true },
   date: { type: String, required: true },
-  time: { type: String, required: true },
+  services: {type: mongoose.Schema.Types.Mixed},
   isAttended: { type: String, default: false },
 });
 
